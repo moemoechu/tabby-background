ae5871ce92b1b43ebb2efc9900483061b425aef6
犬走椛 <moemoechu@yeah.net>
2023-11-07 20:36:28 +0800

    support file picker.

--- src/settingsTab.component.ts
+++ src/settingsTab.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { BackgroundService } from "background.service";
 import { ConfigService } from "tabby-core";
+import { ElectronHostWindow, ElectronService } from "tabby-electron";
 
 /** @hidden */
 @Component({
@@ -21,12 +22,34 @@ import { ConfigService } from "tabby-core";
         width: 100px;
         flex: none;
       }
+
+      .font-family-input {
+        min-width: 150px;
+        // flex: none;
+      }
+
+      .background-input {
+        min-width: 350px;
+        // flex: none;
+      }
     `,
   ],
 })
 export class BackgroundSettingsTabComponent {
-  constructor(public config: ConfigService, private readonly background: BackgroundService) {}
+  constructor(public config: ConfigService, private readonly background: BackgroundService, private electron: ElectronService, private hostWindow: ElectronHostWindow) {}
 
+  async pickFile(): Promise<void> {
+    const paths = (
+      await this.electron.dialog.showOpenDialog(this.hostWindow.getWindow(), {
+        properties: ["openFile", "showHiddenFiles"],
+      })
+    ).filePaths;
+    if (paths[0]) {
+      this.config.store.backgroundPlugin.backgroundPath = paths[0];
+      // this.config.save();
+      this.apply();
+    }
+  }
   apply() {
     this.config.save();
     this.background.applyCss();
